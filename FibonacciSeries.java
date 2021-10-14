c6682b9aa867a90fa2870d3dee4d5e3f2cdd1488
Ravi Kumar <ravikumarsingh9907@gmail.com>
2021-10-14 12:24:51 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -3,11 +3,22 @@ public class Fibonacci {
     // Function Calling----------------------
         System.out.println(series(6));
     }
-
-    static int series(int fnum){ // creating function with argument-----------------------
-         if(fnum < 2) { // Base Condition----------------
-             return fnum;
-         }
-         return series(fnum-1) + series(fnum-2); // Function calling it self--------------------
+// This method is used to find fibonacci series of smaller numbers, You can't calculate Fibo for bigger number with this method (METHOD-1)
+    
+//     static int series(int fnum){ // creating function with argument-----------------------
+//          if(fnum < 2) { // Base Condition----------------
+//              return fnum;
+//          }
+//          return series(fnum-1) + series(fnum-2); // Function calling it self--------------------
+//     }
+ 
+    
+// Used to find Fibo of bigger numbers---------------------(METHOD-2)
+    
+    static int series(int fnum) {
+        return (int)(Math.pow(((1+Math.sqrt(5))/2), fnum)/Math.sqrt(5));
     }
+    
+//finding fibo via recurance------------------(METHOD-3)
+    
 }
